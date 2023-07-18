7e3620df359ad71a34e078a353d4105a6b558b23
Hanna Ivanova <hanna_ivanova@epam.com>
2023-07-18 19:57:23 +0300

    hm6_oop2

--- src/hw6/Clinic/Main.java
+++ src/hw6/Clinic/Main.java
@@ -3,8 +3,10 @@ package hw6.Clinic;
 public class Main {
     public static void main(String[] args) {
         Patient patient1 = new Patient();
+        Patient patient2 = new Patient();
         patient1.setPlan(TreatmentPlan.Second);
         patient1.setDoctor();
+        patient2.setDoctor();
     }
 
 }
